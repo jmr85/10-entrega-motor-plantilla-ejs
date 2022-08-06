ea6ef1019a62e06e2e5f029e9388267f82ac5fa5
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-08-06 04:51:56 -0300

    first commmit

---
+++ README.md
@@ -0,0 +1,1 @@
+Desafio: Motores de plantillas - Motor EJS
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+let products = [];
+
+app.get('/', function (req, res) {
+    res.render('pages/index.ejs');   
+});
+
+app.post('/productos',(req, res) => {
+    const obj  = req.body;//name, price
+    console.log("productos: ", obj);
+    products.push(obj);
+    res.redirect('/');
+})
+
+app.get('/productos', (req, res) => {
+    res.render('pages/productos.ejs', {
+        products: products,
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}).on('error', err => {
+    console.log(err);
+});
